b41e14fe283ee20e20dd4e3e4ca20c608d231b9f
qwertyboy <113260269+dkuandyk@users.noreply.github.com>
2023-03-09 05:07:30 +0600

    Add files via upload

---
+++ game.js
@@ -0,0 +1,39 @@
+import Preloader from './assets/classes/Preloader.js'
+import Main from './assets/classes/Main.js'
+
+let config = {
+    type : Phaser.AUTO,
+    width : 1200,
+    height : 600,
+    scale : {
+        mode : Phaser.Scale.FIT,
+        autoCenter : Phaser.Scale.CENTER_BOTH
+    },
+    physics: {
+        default: 'matter',
+        matter: {
+            //debug: true,
+            gravity: {
+                y: 3,
+            },
+        }
+    },
+    backgroundColor : 0xffffff,
+    scene : [Preloader, Main],
+}
+
+
+let storageData = localStorage.getItem('battle');
+    
+if(storageData) storageData = JSON.parse(storageData);
+else storageData = {};
+
+const Game = new Phaser.Game(config);
+Game.data = storageData;
+      
+Game.saveUserData = function(){
+localStorage.setItem(
+    'battle',
+    JSON.stringify(Game.data)
+    );
+};
